75ad5ab3c3857001e9a7820aa7707a8da316e234
Orin Serimon <orin220444@protonmail.com>
2019-11-10 20:40:17 +0700

    приветственное сообщение

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config({ path: './.env'})
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 const path = require('path')
-
-
+const welcomemessage = `Приветствую тебя, ${ctx.from.admin} в чате: ${ctx.from.chat}`
+bot.on('new_chat_members', welcomemessage)
+      
 
 bot.catch((error) => {
     console.log('Oops', error)
