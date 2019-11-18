55d22986976ac44048c9082a1b3cae4c6dd8ecd0
Orin Serimon <orin220444@protonmail.com>
2019-11-18 17:38:32 +0700

    добавлена команда бана

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ require('dotenv').config({ path: './.env'})
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 const path = require('path')
-const welcomemessage = `Приветствую тебя, ${ctx.from.admin} в чате: ${ctx.from.chat}`
-bot.on('new_chat_members', welcomemessage)
-      
-
+bot.on('new_chat_members', (ctx => (`Приветствую тебя, ${ctx.from.admin} в чате: ${ctx.from.chat}`)))
+  
+  
+  bot.command('ban', (async(ctx) =>  {
+    const chatMember = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id)
+    if (chatMember && ['creator', 'administrator'].includes(chatMember.status)) {
+await telegram.restrictChatMember(ctx.from.chat.id,ctx.message.reply_to_message.from)
+ctx.reply(ctx.from.id, `Пользователь ${ctx.reply.from.id} забанен!`)
+    }else{
+      ctx.reply(`Эта команда доступна только администраторам!`)
+    }
+}))
 bot.catch((error) => {
     console.log('Oops', error)
   })
