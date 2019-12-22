2ae66351d042e09eb4aca37ee714f918bf94cf1b
Orin Serimon <orin220444@protonmail.com>
2019-12-22 11:59:39 +0700

    code base was updated

--- index.js
+++ index.js
@@ -14,54 +14,64 @@ bot.help((ctx) => {
 bot.command('ban', (async (ctx) => {
   const chatMember = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id);
   const banUser = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.reply_to_message.from.id);
-  const ischatMemberAnAdmin = await chatMember.status === 'creator' || 'administrator';
-  const isbanUserAnAdmin = await banUser.status === 'creator' || 'administrator';
-  if (isbanUserAnAdmin == true) {
-    ctx.reply(reply.banUserIsAnAdmin);
-  } if (ischatMemberAnAdmin == false) {
-    ctx.reply(reply.chatMemberIsNotAnAdmin);
-  } else {
-    await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, {
-      can_send_messages: false,
-      can_send_other_messages: false,
-      can_send_media_messages: false,
-      can_add_web_page_previews: false,
-    });
-    await ctx.reply(reply.userBanned);
+  const ischatMemberAnAdmin = await chatMember.status !== 'creator' || 'administrator';
+  const isbanUserAnAdmin = await banUser.status == 'creator' || 'administrator';
+  switch (true) {
+    case isbanUserAnAdmin:
+      ctx.reply(reply.banUserIsAnAdmin);
+      break;
+    case ischatMemberAnAdmin:
+      ctx.reply(reply.chatMemberIsNotAnAdmin);
+      break;
+    default:
+      await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, {
+        can_send_messages: false,
+        can_send_other_messages: false,
+        can_send_media_messages: false,
+        can_add_web_page_previews: false,
+      });
+      await ctx.reply(reply.userBanned);
+      break;
   }
-}));
+}
+));
 bot.command('unban', (async (ctx) => {
   const chatMember = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id);
   const unBanUser = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.reply_to_message.from.id);
-  const ischatMemberAnAdmin = await chatMember.status === 'creator' || 'administrator';
+  const ischatMemberAnAdmin = await chatMember.status !== 'creator' || 'administrator';
   const isbanUserAnAdmin = await unBanUser.status === 'creator' || 'administrator';
-  if (isbanUserAnAdmin == true) {
-    ctx.reply(reply.banUserIsAnAdmin);
-  }
-  if (ischatMemberAnAdmin == false) {
-    ctx.reply(reply.chatMemberIsNotAnAdmin);
-  } else {
-    await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, {
-      can_send_messages: true,
-      can_send_other_messages: true,
-      can_send_media_messages: true,
-      can_add_web_page_previews: true,
-    });
-    await ctx.reply(reply.userUnbanned);
+  switch (true) {
+    case isbanUserAnAdmin:
+      ctx.reply(reply.banUserIsAnAdmin);
+      break;
+    case ischatMemberAnAdmin:
+      ctx.reply(reply.chatMemberIsNotAnAdmin);
+      break;
+    default:
+      await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, {
+        can_send_messages: true,
+        can_send_other_messages: true,
+        can_send_media_messages: true,
+        can_add_web_page_previews: true,
+      });
+      await ctx.reply(reply.userUnbanned);
   }
 }));
 bot.command('kick', (async (ctx) => {
   const chatMember = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id);
   const kickUser = await ctx.telegram.getChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id);
-  const ischatMemberAnAdmin = await chatMember.status === 'creator' || 'administrator';
+  const ischatMemberAnAdmin = await chatMember.status !== 'creator' || 'administrator';
   const iskickUserAnAdmin = await kickUser.status === 'creator' || 'administrator';
-  if (iskickUserAnAdmin == true) {
-    ctx.reply(reply.kickUserIsAnAdmin);
-  } if (ischatMemberAnAdmin == false) {
-    ctx.reply(reply.chatMemberIsNotAnAdmin);
-  } else {
-    await ctx.telegram.kickChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id);
-    await ctx.reply(reply.userKicked);
+  switch (true) {
+    case iskickUserAnAdmin:
+      ctx.reply(reply.kickUserIsAnAdmin);
+      break;
+    case ischatMemberAnAdmin:
+      ctx.reply(reply.chatMemberIsNotAnAdmin);
+      break;
+    default:
+      await ctx.telegram.kickChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id);
+      await ctx.reply(reply.userKicked);
   }
 }));
 
