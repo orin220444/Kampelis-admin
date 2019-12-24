6b00bb1d29ffa2ec264d083996f5ff884caa3790
Orin Serimon <orin220444@protonmail.com>
2019-12-24 17:52:35 +0700

    minor bug fixes

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ bot.use(Telegraf.session());
 bot.use(i18n.middleware());
 bot.on('new_chat_members', (ctx) => {
   answer = ctx.i18n.t('newChatMembers');
-  ctx.replyWithHTML(answer);
+  ctx.reply(answer);
 });
 bot.help((ctx) => {
-  ctx.reply(reply.help);
+  ctx.reply.i18n.t('help');
 });
 bot.command('test', (ctx) => {
   answer = ctx.i18n.t('test');
-  ctx.replyWithHTML(answer);
+  ctx.reply(answer);
 });
 bot.command('ban', (async (ctx) => {
   const chatMember = await ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id);
@@ -28,9 +28,9 @@ bot.command('ban', (async (ctx) => {
   const ischatMemberAnAdmin = await chatMember.status === 'creator' || 'administrator';
   const isbanUserAnAdmin = await banUser.status === 'creator' || 'administrator';
   if (isbanUserAnAdmin == true) {
-    ctx.replyWithHTML.i18n.t('banUserIsAnAdmin');
+    ctx.reply.i18n.t('banUserIsAnAdmin');
   } if (ischatMemberAnAdmin == false) {
-    ctx.replyWithHTML.i18n.t('chatMemberIsNotAnAdmin');
+    ctx.reply.i18n.t('chatMemberIsNotAnAdmin');
   } else {
     await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, {
       can_send_messages: false,
@@ -39,7 +39,7 @@ bot.command('ban', (async (ctx) => {
       can_add_web_page_previews: false,
     });
     answer = ctx.i18n.t('userBanned');
-    await ctx.replyWithHTML(answer);
+    await ctx.reply(answer);
   }
 }));
 bot.command('unban', (async (ctx) => {
@@ -49,11 +49,11 @@ bot.command('unban', (async (ctx) => {
   const isbanUserAnAdmin = await unBanUser.status === 'creator' || 'administrator';
   if (isbanUserAnAdmin == true) {
     const answer = ctx.i18n.t('banUserIsAnAdmin');
-    ctx.replyWithHTML(answer);
+    ctx.reply(answer);
   }
   if (ischatMemberAnAdmin == false) {
     const answer = ctx.i18n.t('chatMemberIsNotAnAdmin');
-    ctx.replyWithHTML(answer);
+    ctx.reply(answer);
   } else {
     await ctx.telegram.restrictChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id, {
       can_send_messages: true,
@@ -62,7 +62,7 @@ bot.command('unban', (async (ctx) => {
       can_add_web_page_previews: true,
     });
     const answer = ctx.i18n.t('userUnbanned');
-    await ctx.replyWithHTML(answer);
+    await ctx.reply(answer);
   }
 }));
 bot.command('kick', (async (ctx) => {
@@ -72,14 +72,14 @@ bot.command('kick', (async (ctx) => {
   const iskickUserAnAdmin = await kickUser.status === 'creator' || 'administrator';
   if (iskickUserAnAdmin == true) {
     ctx.i18n.t('kickUserIsAnAdmin');
-    ctx.replyWithHTML(answer);
+    ctx.reply(answer);
   } if (ischatMemberAnAdmin == false) {
     const answer = ctx.i18n.t('chatMemberIsNotAnAdmin');
-    ctx.replyWithHTML(answer);
+    ctx.reply(answer);
   } else {
     await ctx.telegram.kickChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id);
     answer = ctx.i18n.t('userKicked');
-    await ctx.replyWithHTML(answer);
+    await ctx.reply(answer);
   }
 }));
 
@@ -87,10 +87,10 @@ bot.use((ctx) => {
   console.log(ctx.message);
 });
 
-bot.catch((error, (ctx) => {
+bot.catch((error, ctx) => {
   console.log('Oops', error);
   ctx.telegram.sendMessage(364841884, error);
-}));
+});
 bot.launch().then(() => {
   console.log(`bot started`);
 });
