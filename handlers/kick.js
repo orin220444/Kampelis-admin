f742226758604be16c52736e165dbe1cd3a20d02
Orin Serimon <orin220444@protonmail.com>
2020-01-18 22:11:45 +0700

    bug fixes and little improvements

--- handlers/kick.js
+++ handlers/kick.js
@@ -1,55 +1,60 @@
 module.exports = async (ctx) => {
-  const chatMember = await ctx.telegram.getChatMember(
-      ctx.message.chat.id, ctx.message.from.id,
-  );
-  const kickUser = await ctx.telegram.getChatMember(
-      ctx.chat.id, ctx.message.reply_to_message.from.id,
-  );
-  const ischatMemberAnAdmin =
+  if (!ctx.message.reply_to_message) {
+    const answer = ctx.i18n.t('nobodytokick');
+    ctx.reply(answer,
+        {reply_to_message_id: ctx.message.message_id});
+  } else {
+    const chatMember = await ctx.telegram.getChatMember(
+        ctx.message.chat.id, ctx.message.from.id,
+    );
+    const kickUser = await ctx.telegram.getChatMember(
+        ctx.chat.id, ctx.message.reply_to_message.from.id,
+    );
+    const ischatMemberAnAdmin =
     await chatMember.status === 'creator' || 'administrator';
-  const iskickUserAnAdmin =
+    const iskickUserAnAdmin =
     await kickUser.status === 'creator' || 'administrator';
 
-
-  if (iskickUserAnAdmin == true) {
-    try {
-      const answer = ctx.i18n.t('kickUserIsAnAdmin');
-      ctx.reply(answer, {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
+    if (iskickUserAnAdmin == true) {
+      try {
+        const answer = ctx.i18n.t('kickUserIsAnAdmin');
+        ctx.reply(answer, {reply_to_message_id: ctx.message.message_id},
+        );
+      } catch (error) {
+        const answer = ctx.i18n.t('error', {error: error});
+        ctx.replyWithMarkdown(answer,
+            {reply_to_message_id: ctx.message.message_id},
+        );
+      }
     }
-  }
-  if (ischatMemberAnAdmin == false) {
-    try {
-      const answer = ctx.i18n.t('chatMemberIsNotAnAdmin');
-      ctx.reply(answer, {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
+    if (ischatMemberAnAdmin == false) {
+      try {
+        const answer = ctx.i18n.t('chatMemberIsNotAnAdmin');
+        ctx.reply(answer, {reply_to_message_id: ctx.message.message_id},
+        );
+      } catch (error) {
+        const answer = ctx.i18n.t('error', {error: error});
+        ctx.replyWithMarkdown(answer,
+            {reply_to_message_id: ctx.message.message_id},
+        );
+      }
+    } else {
+      try {
+        await ctx.telegram.kickChatMember(ctx.chat.id, kickUser.user.id);
+        await ctx.telegram.unbanChatMember(ctx.chat.id, kickUser.user.id);
+        const answer = await ctx.i18n.t('userKicked', {
+          user: kickUser.first_name,
+          user_id: kickUser.id,
+        });
+        await ctx.replyWithMarkdown(answer,
+            {reply_to_message_id: ctx.message.message_id},
+        );
+      } catch (error) {
+        const answer = ctx.i18n.t('error', {error: error});
+        ctx.replyWithMarkdown(answer,
+            {reply_to_message_id: ctx.message.message_id},
+        );
+      }
     }
-  } else {
-    try {
-      await ctx.telegram.kickChatMember(ctx.chat.id, kickUser.id);
-      await ctx.telegram.unbanChatMember(ctx.chat.id, kickUser.id);
-      const answer = await ctx.i18n.t('userKicked', {
-        user: kickUser.first_name,
-        user_id: kickUser.id,
-      });
-      await ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    }
-  }
+  };
 };
--- handlers/setrules.js
+++ handlers/setrules.js
@@ -24,3 +24,4 @@ module.exports = async (ctx) => {
     }
   }
 };
+// TODO: check for the admin
