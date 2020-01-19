ea05650527326518da9c5c8ca7eddbeb63e7e7d0
Orin Serimon <orin220444@protonmail.com>
2020-01-19 16:50:15 +0700

    added registering chat to /reg

--- handlers/reg.js
+++ handlers/reg.js
@@ -1,6 +1,7 @@
-const {User} = require('../database.js');
+const {User, Group} = require('../database.js');
 module.exports = async (ctx) => {
   const user = await User.findOne({id: ctx.from.id});
+  const group = await Group.findOne({group_id: ctx.chat.id});
 
   if (!user) {
     try {
@@ -20,16 +21,46 @@ module.exports = async (ctx) => {
           {reply_to_message_id: ctx.message.message_id},
       );
     }
-  };
-  try {
-    const answer = ctx.i18n.t('user.exists');
-    ctx.reply(answer,
-        {reply_to_message_id: ctx.message.message_id},
-    );
-  } catch (error) {
-    const answer = ctx.i18n.t('error', {error: error});
-    ctx.replyWithMarkdown(answer,
-        {reply_to_message_id: ctx.message.message_id},
-    );
+  } else {
+    try {
+      const answer = ctx.i18n.t('user.exists');
+      ctx.reply(answer,
+          {reply_to_message_id: ctx.message.message_id},
+      );
+    } catch (error) {
+      const answer = ctx.i18n.t('error', {error: error});
+      ctx.replyWithMarkdown(answer,
+          {reply_to_message_id: ctx.message.message_id},
+      );
+    }
+  }
+  if (!group) {
+    try {
+      const group = await Group.create({
+        group_id: ctx.chat.id,
+        title: ctx.chat.title,
+
+      });
+      await group.save();
+      const answer = ctx.i18n.t('group.added');
+      ctx.reply(answer, {reply_to_message_id: ctx.message.message_id});
+    } catch (error) {
+      const answer = ctx.i18n.t('error', {error: error});
+      ctx.replyWithMarkdown(answer,
+          {reply_to_message_id: ctx.message.message_id},
+      );
+    }
+  } else {
+    try {
+      const answer = ctx.i18n.t('group.exists');
+      ctx.reply(answer,
+          {reply_to_message_id: ctx.message.message_id},
+      );
+    } catch (error) {
+      const answer = ctx.i18n.t('error', {error: error});
+      ctx.replyWithMarkdown(answer,
+          {reply_to_message_id: ctx.message.message_id},
+      );
+    }
   }
 };
