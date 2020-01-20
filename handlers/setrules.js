81923f7d2bdd18b065ad3f495b1b684fc5d8debf
Orin Serimon <orin220444@protonmail.com>
2020-01-21 02:44:30 +0700

    check for the admin

--- handlers/setrules.js
+++ handlers/setrules.js
@@ -1,6 +1,12 @@
 const {Group} = require('../database');
 module.exports = async (ctx) => {
   const group = await Group.findOne({group_id: ctx.chat.id});
+  const chatMember = await ctx.telegram.getChatMember(
+      ctx.message.chat.id, ctx.message.from.id,
+  );
+  const ischatMemberAnAdmin =
+await chatMember.status === 'creator' || 'administrator';
+
   if (!group) {
     try {
       const answer = ctx.i18n.t('group.notfound');
@@ -14,6 +20,8 @@ module.exports = async (ctx) => {
       );
     }
   } else {
+    console.log(chatMember);
+    console.log(ischatMemberAnAdmin);
     if (!ctx.message.reply_to_message) {
       try {
         const answer = ctx.i18n.t('setrule.err');
@@ -26,21 +34,34 @@ module.exports = async (ctx) => {
         );
       }
     }
-    if (ctx.message.reply_to_message) {
-      try {
-        group.rules = await ctx.message.reply_to_message.text;
-        await group.save();
-        const answer = ctx.i18n.t('setrule.suc');
-        await ctx.reply(answer,
-            {reply_to_message_id: ctx.message.message_id},
-        );
-      } catch (error) {
-        const answer = ctx.i18n.t('error', {error: error});
-        ctx.replyWithMarkdown(answer,
-            {reply_to_message_id: ctx.message.message_id},
-        );
+    if (ischatMemberAnAdmin) {
+      if (ctx.message.reply_to_message) {
+        try {
+          group.rules = await ctx.message.reply_to_message.text;
+          await group.save();
+          const answer = ctx.i18n.t('setrule.suc');
+          await ctx.reply(answer,
+              {reply_to_message_id: ctx.message.message_id},
+          );
+        } catch (error) {
+          const answer = ctx.i18n.t('error', {error: error});
+          ctx.replyWithMarkdown(answer,
+              {reply_to_message_id: ctx.message.message_id},
+          );
+        }
+      } else {
+        try {
+          const answer = ctx.i18n.t('setrule.notaadmin');
+          ctx.replyWithMarkdown(answer,
+              {reply_to_message_id: ctx.message.message_id},
+          );
+        } catch (error) {
+          const answer = ctx.i18n.t('error', {error: error});
+          ctx.replyWithMarkdown(answer,
+              {reply_to_message_id: ctx.message.message_id},
+          );
+        }
       }
     }
   };
 };
-// FIXME: check for the admin
