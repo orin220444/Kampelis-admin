0343ca1e03be0dcd41872beba626393a4a2dbc43
Orin Serimon <orin220444@protonmail.com>
2020-01-20 07:14:30 +0700

    Revert "WIP: gifid now saves gifs in the db"
    This reverts commit 8ac66e1003ecb37f2c6e7afcb99f373c61f76eec.

--- handlers/gifid.js
+++ handlers/gifid.js
@@ -1,47 +1,10 @@
-const {Group} = require('../database');
-module.exports = async (ctx) => {
-  const group = await Group.findOne({group_id: ctx.chat.id});
-  if (!group) {
-    try {
-      const answer = ctx.i18n.t('group.notfound');
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    }
-  } if (!ctx.message.reply_to_message) {
-    try {
-      const answer = ctx.i18n.t('nogif');
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    }
-  } else {
-    try {
-      console.log(ctx.message.reply_to_message.animation.file_id);
-      console.log(group.gifs);
-      group.gifs = await `${ctx.message.reply_to_message.animation.file_id}`;
-      console.log(group.gifs);
-      await group.save();
-      console.log(group.gifs);
-      const answer = ctx.i18n.t('setrule.suc');
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    }
-  };
+module.exports = (ctx) => {
+  try {
+    console.log(ctx.message.reply_to_message.animation.file_id);
+  } catch (error) {
+    const answer = ctx.i18n.t('error', {error: error});
+    ctx.replyWithMarkdown(answer,
+        {reply_to_message_id: ctx.message.message_id},
+    );
+  }
 };
