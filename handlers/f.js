7ae74c893b4703543e182853d343c5fd57125832
Orin Serimon <orin220444@protonmail.com>
2020-01-21 02:22:47 +0700

    minor bug fixes

--- handlers/f.js
+++ handlers/f.js
@@ -2,10 +2,7 @@ const gifs = require('../config/gifs.json');
 module.exports = (ctx) => {
   try {
     const gif = gifs.f;
-    console.log(gif);
-    console.log(gif.length);
     const randomGif = gif[Math.floor(Math.random() * gif.length)];
-    console.log(randomGif);
     ctx.replyWithDocument(randomGif,
         {reply_to_message_id: ctx.message.message_id},
     );
