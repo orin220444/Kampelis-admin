123ccfcadaf1473c5a5f225458780e2c3a8bb367
Orin Serimon <orin220444@protonmail.com>
2020-02-05 17:18:43 +0700

    minor bug fixes

--- handlers/id.js
+++ handlers/id.js
@@ -1,10 +1,10 @@
 module.exports = (ctx) => {
-  if (!ctx.message.reply_to_message) {
+  if (ctx.message.reply_to_message) {
     try {
       ctx.reply(ctx.message.reply_to_message,
-          {reply_to_message_id: ctx.message.message.id});
+          {reply_to_message_id: ctx.message.message_id});
     } catch (error) {
-      const answer = ctx.i18n.t(error, {error: error});
+      const answer = ctx.i18n.t('error', {error: error});
       ctx.replyWithMarkdown(answer);
     }
   }
