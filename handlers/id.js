ad9a39e8e00030ae6e1cc1ba48d51fb5e1207d74
Orin Serimon <orin220444@protonmail.com>
2020-02-05 18:28:54 +0700

    little improvements

--- handlers/id.js
+++ handlers/id.js
@@ -1,11 +1,31 @@
 module.exports = (ctx) => {
-  if (ctx.message.reply_to_message) {
+  if (!ctx.message.reply_to_message) {
     try {
-      ctx.reply(ctx.message.reply_to_message,
-          {reply_to_message_id: ctx.message.message_id});
+      const answer = ctx.i18n.t('id.err');
+      ctx.reply(answer);
     } catch (error) {
       const answer = ctx.i18n.t('error', {error: error});
       ctx.replyWithMarkdown(answer);
+    }
+  } else {
+    if (ctx.message.reply_to_message.sticker) {
+      try {
+        ctx.reply(ctx.message.reply_to_message.sticker,
+            {reply_to_message_id: ctx.message.message_id});
+      } catch (error) {
+        const answer = ctx.i18n.t('error', {error: error});
+        ctx.replyWithMarkdown(answer);
+      }
+    }
+
+    if (ctx.message.reply_to_message.animation) {
+      try {
+        ctx.reply(ctx.message.reply_to_message.animation,
+            {reply_to_message_id: ctx.message.message_id});
+      } catch (error) {
+        const answer = ctx.i18n.t('error', {error: error});
+        ctx.replyWithMarkdown(answer);
+      }
     }
   }
 };
