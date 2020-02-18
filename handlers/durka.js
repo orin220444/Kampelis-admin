2beb6968baa1a912b790a70a6573ddb9ff39ff6a
Orin Serimon <orin220444@protonmail.com>
2020-02-19 00:17:43 +0700

    https://trello.com/c/bUZoah3h

--- handlers/durka.js
+++ handlers/durka.js
@@ -2,27 +2,19 @@ const files = require('../config/files.json');
 module.exports = (ctx) => {
   const sticker = files.durka;
   const randomSticker = sticker[Math.floor(Math.random() * sticker.length)];
-  if (!ctx.message.reply_to_message) {
-    try {
-      ctx.replyWithSticker(randomSticker,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    }
-  } else {
-    try {
-      ctx.replyWithSticker(randomSticker,
-          {reply_to_message_id: ctx.message.reply_to_message.message_id},
-      );
-    } catch (error) {
-      const answer = ctx.i18n.t('error', {error: error});
-      ctx.replyWithMarkdown(answer,
-          {reply_to_message_id: ctx.message.message_id},
-      );
-    }
+  try {
+    const isReply = !!ctx.message.reply_to_message;
+    const message = isReply ?
+      ctx.message.reply_to_message.message_id :
+      ctx.message.message_id;
+    ctx.replyWithSticker(randomSticker,
+        {reply_to_message_id: message},
+    );
+  } catch (error) {
+    const answer = ctx.i18n.t('error', {error: error});
+    ctx.replyWithMarkdown(answer,
+        {reply_to_message_id: ctx.message.message_id},
+    );
   }
 };
+// FIXME
