94d5e581cd70a028296ee547586f90339c969bde
Orin Serimon <orin220444@protonmail.com>
2020-02-19 00:37:37 +0700

    rewritten using the ternary operator

--- handlers/f.js
+++ handlers/f.js
@@ -3,8 +3,12 @@ module.exports = (ctx) => {
   try {
     const gif = files.f;
     const randomGif = gif[Math.floor(Math.random() * gif.length)];
+    const isReply = !!ctx.message.reply_to_message;
+    const message = isReply ?
+      ctx.message.reply_to_message.message_id :
+      ctx.message.message_id;
     ctx.replyWithDocument(randomGif,
-        {reply_to_message_id: ctx.message.message_id},
+        {reply_to_message_id: message},
     );
   } catch (error) {
     const answer = ctx.i18n.t('error', {error: error});
