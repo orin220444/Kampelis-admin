5192b8f5b94e5a7db0fcb5f72dec6991c9449706
Orin Serimon <orin220444@protonmail.com>
2020-11-01 18:30:32 +0700

    WIP:fix __dirname is not defined

--- bot/i18n.js
+++ bot/i18n.js
@@ -1,5 +1,10 @@
 import I18n from 'telegraf-i18n';
 import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 export const i18n = new I18n({
   directory: path.resolve(__dirname, '../locales'),
   defaultLanguage: 'ru',
